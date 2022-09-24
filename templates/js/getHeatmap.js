f73edd6059ebe7220b614bbb58867c82432ac033
Spencer Ferguson-Dryden <sfergusond@gmail.com>
2022-09-24 10:32:39 -0600

    [hotfix] 502 error on heatmap request

--- templates/js/getHeatmap.js
+++ templates/js/getHeatmap.js
@@ -1,24 +1,38 @@
-fetch('{% url "getHeatmap" %}', {
-  method: 'POST',
-  headers: {
-  'Content-Type': 'application/json',
-  'X-CSRFToken': '{{ csrf_token }}'
-  },
-  body: JSON.stringify({
-    'athlete': {{ request.athlete.id }}
-  })
-})
-.then((response) => {
-  return response.text();
-})
-.then((html) => {
-  const graphElem = document.getElementById('heatmap');
-  graphElem.innerHTML = html;
-  const scripts = graphElem.getElementsByTagName('script')
-  for (var i = 0; i < scripts.length; i++) {
-    eval(scripts[i].innerHTML);
+const getHeatmap = async (graphElem) => {
+  try {
+    const res = await fetch('{% url "getHeatmap" %}', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': '{{ csrf_token }}'
+      },
+      body: JSON.stringify({
+        'athlete': {{ request.athlete.id }}
+      })
+    });
+    if (res.ok) {
+      const html = await res.text();
+      graphElem.innerHTML = html;
+      const scripts = graphElem.getElementsByTagName('script');
+      for (var i = 0; i < scripts.length; i++) {
+        eval(scripts[i].innerHTML);
+      }
+      return true;
+    }
+    return false;
   }
-})
-.catch((error) => {
-  console.log(error);
-});
+  catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+const graphElem = document.getElementById('heatmap');
+graphElem.innerHTML = '<h3 class="text-center">Loading...</h3>';
+(async () => {
+  let success = await getHeatmap(graphElem);
+  while (!success) {
+    graphElem.innerHTML = '<h3 class="text-center">Loading...</h3>';
+    success = getHeatmap(graphElem);
+  }
+})();
